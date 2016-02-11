7a04e3011936cfd88899cf6b3d04d7f63dafe22e
Anand Kumar Chaudhary <anand.kmk@gmail.com>
2016-02-11 17:45:12 +0530

    farji

--- parts/notices/farji.php
+++ parts/notices/farji.php
@@ -4,5 +4,4 @@ Notice ID: anand_farji_notice
 Notice Type: update
 Capability: manage_options
 */
-
-echo 'Wonder I never realised that i am this much power full!!! :D :D';
+_e( 'Updatedhehehehehe!', 'my-text-domain' ); ?>
