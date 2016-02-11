a76a4818d79a711a35d885a747e770a192f8cea2
Anand Kumar Chaudhary <anand.kmk@gmail.com>
2016-02-11 17:27:53 +0530

    added an index.php
    basic plugin page to set

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: [WOO] delivery boy extension
+Plugin URI: http://example.com
+Description: An addon for adding delivery boy types.
+Version: 0.1
+Author: Anand Kumar Chaudhary
+Author URI: http://about.me/anand.kmk
+Plugin Type: Piklist
+License: A "Slug" license name e.g. GPL2
+*/
